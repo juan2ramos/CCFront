9b4348da775911775588e1bd2d2f685a5ac0c164
juan2ramos <juan2ramos@gmail.com>
2014-02-19 19:18:55 -0500

    resestructuracion

--- js/index.js
+++ js/index.js
@@ -16,34 +16,110 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ var xhReq = new XMLHttpRequest(),
+     inicioConf = {
+        'ciudad'      : 'Ciudad',
+        'cc'          : 'Centro Comercial',
+        'categoria'   : 'Categoria' 
+     };
+
 var app = {
     // Application Constructor
+    
     initialize: function() {
         this.bindEvents();
+
+        
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady, true);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
+        FastClick.attach(document.body);
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
+
     receivedEvent: function(id) {
+        console.log('Received Event: ' + id)
+        xhReq.open("GET", "inicio.html", false);
+        xhReq.send(null);
+        document.getElementById("content-page").innerHTML=xhReq.responseText;
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
+        
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
+
+    }  
+
+
 };
+function select(dato, sitio){
+
+    xhReq.open("GET", "inicio.html", false);
+    xhReq.send(null);
+    document.getElementById("content-page").innerHTML=xhReq.responseText;
+    inicioConf[sitio]=dato;
+    habilitarSubmit();
+    agregarInicio();
+}
+function menu(opcion){ 
+    xhReq.open("GET", opcion+".html", false);
+    xhReq.send(null);
+
+    document.getElementById("titulo").innerHTML='<h2>'+opcion+'<h2';
+    document.getElementById("content-page").innerHTML=xhReq.responseText;
+}
+function info(){
+    menu('infoTienda');
+    document.getElementById("titulo").innerHTML='<figure id="logo"><img  src="img/imagotipo.png"></figure>'
+    
+}
+function agregarInicio(){
+    document.getElementById("titulo").innerHTML='<figure id="logo"><img  src="img/imagotipo.png"></figure>'
+    for (var key in inicioConf) {
+        document.getElementById(key).innerHTML=inicioConf[key];
+    }
+} 
+function submitSearch(thisButton){
+    if(habilitarSubmit()){
+        menu('tiendas');
+        document.getElementById('shop').value = 'Info ' + inicioConf['cc'];
+    }
+}
+function habilitarSubmit(){
+    if (inicioConf['ciudad'] != 'Ciudad' && inicioConf['cc'] != 'Centro Comercial' && inicioConf['categoria'] != 'Categoria' ){
+       addClass('active' , document.getElementById('search'));
+       removeClass('hidden',document.getElementById('contend-search'));
+       return true ;
+    }
+    return false;
+}
+
+function addClass( classname, element ) {
+    var cn = element.className;
+    if( cn.indexOf( classname ) != -1 ) {
+        return;
+    }
+    if( cn != '' ) {
+        classname = ' '+classname;
+    }
+    element.className = cn+classname;
+}
+function removeClass( classname, element ) {
+    var cn = element.className;
+    var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
+    cn = cn.replace( rxp, '' );
+    element.className = cn;
+}
